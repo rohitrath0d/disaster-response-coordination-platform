7389206fa1022825de19de9efc34706c6d281fb3
Rohit Rathod <122153964+rohitrath0d@users.noreply.github.com>
2025-07-02 22:04:16 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -36,7 +36,8 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<App />} /> */}
-        <Route path="/home" element={<HomePage />} />
+{/*         <Route path="/home" element={<HomePage />} /> */}
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
 
         {/* <Route
